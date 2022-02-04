d31d4f918edd9784811c182614f0971bd8a31dcd
Andy8131 <andy.liu0123456@gmail.com>
2022-02-03 22:47:27 -0800

    fixed test1

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void test1(){
-        assertEquals(2, SkillDemo.subtract(10, 2));
+        assertEquals(8, SkillDemo.subtract(10, 2));
     }
 }
